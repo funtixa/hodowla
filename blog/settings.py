dcee88a6269718fb50db1cea9a27583853033d4e
Your Name <funtixa@gmail.com>
2020-01-15 20:42:42 +0100

    Base_dirchanged

--- blog/settings.py
+++ blog/settings.py
@@ -167,9 +167,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static_in_env')]
-VENV_PATH = os.path.dirname(BASE_DIR)
+# VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
 # Tinymce
