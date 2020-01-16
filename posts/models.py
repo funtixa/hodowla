cb275ee04bdc6261145763dcbfe25edd6f67aa1d
Your Name <funtixa@gmail.com>
2020-01-16 21:05:06 +0100

    tinymcecheck

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,4 @@
-from tinymce.models import HTMLField
+from tinymce import HTMLField
 # from tinymce import models as tinymce_models
 from django.db import models
 from django.contrib.auth import get_user_model
@@ -45,7 +45,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     overview = models.TextField(max_length=170)
     timestamp = models.DateTimeField(auto_now_add=True)
-    content = HTMLField
+    content = HTMLField('Content')
     # comment_count = models.IntegerField(default = 0)
     # view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
