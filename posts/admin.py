164bfe67fa246aca63ed3eb3c1772e6ecf8010b0
Your Name <funtixa@gmail.com>
2020-01-27 23:05:12 +0100

    admin modded comment

--- posts/admin.py
+++ posts/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 from .models import Author, Category, Post, Comment, PostView
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user','timestamp','content','post')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'user',
+                'timestamp',
+                'content',
+                'post'
+            ),
+        }),
+    )
+    
+
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(PostView)
+
